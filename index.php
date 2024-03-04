d4cd7f3313a421709156e47dfbdaf087b324eee6
De Torres <detorrez@yandex.ru>
2024-03-04 16:09:26 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+// Установите свой API ключ Todoist
+$apiToken = '73c9a99c49718d9fcd29c6f61253b1c4b77af3e7';
+
+// URL для получения списка проектов
+$url = 'https://api.todoist.com/rest/v1/projects';
+
+// Настройка запроса
+$options = array(
+    'http' => array(
+        'header'  => "Authorization: Bearer " . $apiToken,
+        'method'  => 'GET',
+    ),
+);
+
+// Создание контекста запроса
+$context  = stream_context_create($options);
+
+// Выполнение запроса и получение ответа
+$response = file_get_contents($url, false, $context);
+
+// Преобразование ответа в массив
+$projects = json_decode($response, true);
+
+// Вывод списка проектов
+if (!empty($projects)) {
+    echo "Список проектов в Todoist:\n";
+    foreach ($projects as $project) {
+        echo "- {$project['name']}\n";
+    }
+} else {
+    echo "Список проектов пуст\n";
+}
